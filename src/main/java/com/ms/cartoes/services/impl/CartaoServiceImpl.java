9af8c03b83ac6d857c1359ea5f4d4b2b32a7be79
sergiodiogoti <sergiodiogoti@gmail.com>
2025-04-12 09:10:10 -0300

    ajuste no codigo

--- src/main/java/com/ms/cartoes/services/impl/CartaoServiceImpl.java
+++ src/main/java/com/ms/cartoes/services/impl/CartaoServiceImpl.java
@@ -7,11 +7,6 @@ import com.ms.cartoes.repositories.CartaoRepository;
 import com.ms.cartoes.services.CartaoService;
 import com.ms.cartoes.utils.CartaoUtils;
 import jakarta.transaction.Transactional;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,8 +20,6 @@ public class CartaoServiceImpl implements CartaoService {
     final CartaoRepository cartaoRepository;
     final RestTemplate restTemplate;
 
-    @Value("${emissor.url}")
-    private String emissorUrl;
 
     public CartaoServiceImpl(CartaoRepository cartaoRepository, RestTemplate restTemplate) {
         this.cartaoRepository = cartaoRepository;
@@ -65,12 +58,6 @@ public class CartaoServiceImpl implements CartaoService {
             cartaoDTO.setLimite(cartao.getLimite());
             cartaoDTO.setEmail(cartao.getProposta().getCliente().getEmail());
             cartaoDTO.setNomeCliente(cartao.getProposta().getCliente().getNome());
-
-            // Envia os dados para o microserviço Emissor.
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            HttpEntity<CartaoDTO> request = new HttpEntity<>(cartaoDTO, headers);
-            restTemplate.postForEntity(emissorUrl + "/emissor", request, Void.class);
         }
     }
 }
